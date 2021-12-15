87e05edfd0e512a621d096c67b5c6605c88447f7
Andrey <ethenaut@gmail.com>
2021-12-15 17:32:34 +0400

    impl modules

--- 3-realtime-app/m1/m1.cc
+++ 3-realtime-app/m1/m1.cc
@@ -1,7 +1,159 @@
 #include <cstdlib>
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <limits.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/neutrino.h>
+#include <sys/netmgr.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <signal.h>
+
+// Возвращает результат функции
+double getFunctionResult(double t);
 
 int main(int argc, char *argv[]) {
-	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
+	// Если не задан адрес именованной памяти, то завершаем выполненние
+	if (argc < 2)
+	{
+		std::cout << "Incorrect number of arguments." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::cout << "Module M1 is running" << std::endl;
+
+	int const START_TIMER_CODE = -10;
+	int const START_READ_CODE = -11;
+
+	// Получаем id родительского процесса
+	pid_t ppid = getppid();
+
+	// Размер именнованной памяти
+	size_t sharedMemoryLength = 32;
+
+	// Присоединяем именнованную область к процессу
+	int fd = shm_open("/sharemap", O_CREAT | O_RDWR | O_TRUNC, 0);
+	if (fd == -1)
+	{
+		std::cout << "The error occurred while creating share memory." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	ftruncate(fd, sharedMemoryLength);
+
+	// Оображаем именованную память в адресное пространство процесса
+	char *addr = (char*)mmap(0, sharedMemoryLength, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (addr == MAP_FAILED)
+	{
+		std::cout << "The error occurred while mapping shared memory." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Создаем канал
+	int chid;
+	if ((chid = ChannelCreate(0)) == -1)
+	{
+		std::cout << "The error occurred while creating channel." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Устанавливаем соединение с собственным каналом
+	int coid = 0;
+	if ((coid = ConnectAttach(ND_LOCAL_NODE, 0, chid, _NTO_SIDE_CHANNEL, 0)) == -1)
+	{
+		std::cout << "The error occurred while connecting to channel." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Тип уведомления таймера
+	struct sigevent event;
+
+	// Инициализируем уведомление типа "Импульс"
+	SIGEV_PULSE_INIT(&event, coid, SIGEV_PULSE_PRIO_INHERIT, -1, 0);
+
+	// Id таймера
+	timer_t timerid;
+
+	// Создаем таймер
+	if (timer_create(CLOCK_REALTIME, &event, &timerid) == -1)
+	{
+		std::cout << "The error occurred while creating timer." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Свойства таймера
+	struct itimerspec timer;
+
+	// Задаем свойства таймера
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_nsec = 50000000;
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_nsec = 50000000;
+
+	char msg[32]; // сообщение импульса
+	double functionResult = 0; // результат функции
+	char buffer[sharedMemoryLength]; // буффер для записи результата функции
+	double time = 0; // время
+
+	// Запускаем таймер на запись значения функции в буфер
+	timer_settime(timerid, 0, &timer, NULL);
+
+	// Отправляем сигнал родительскому процессу о том, что он может запускать таймер
+	if (SignalKill(ND_LOCAL_NODE, ppid, 0, SIGUSR1, START_TIMER_CODE, 0))
+	{
+		std::cout << "The error occurred while sending START_TIMER signal." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	while (true)
+	{
+		// Возвращаем результат функции
+		functionResult = getFunctionResult(time);
+
+		// Преобразуем результат функции к строке
+		sprintf(buffer, "%f", functionResult);
+
+		//std::cout << "write: " << buffer << std::endl;
+
+		// Копируем результат функции в именованную память
+		strcpy(addr, buffer);
+
+		if (time == 0)
+		{
+			// Отправляем сигнал родительскому процессу о том, что он может начинать читать
+			if (SignalKill(ND_LOCAL_NODE, ppid, 0, SIGUSR1, START_READ_CODE, 0))
+			{
+				std::cout << "The error occurred while sending READY_TO_START_READ signal." << std::endl;
+				std::cout << strerror(errno) << std::endl;
+				return EXIT_FAILURE;
+			}
+		}
+
+		//std::cout << buffer << std::endl;
+
+		// Ожидаем импульс
+		MsgReceivePulse(chid, &msg, sizeof(msg), NULL);
+
+		// Увеличиваем время
+		time += 0.05;
+	}
+
 	return EXIT_SUCCESS;
+}
+
+double getFunctionResult(double t)
+{
+	return sqrt(1 + 4.2 * t + 1.5 * cos(t));
 }
--- 3-realtime-app/m2/m2.cc
+++ 3-realtime-app/m2/m2.cc
@@ -1,7 +1,224 @@
 #include <cstdlib>
+#include <stdio.h>
 #include <iostream>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <iostream>
+#include <signal.h>
+
+// Записать значение функции и время в файл
+void writeToFile(char* functionValue, char* time);
+
+// Обработать пользовательские сигналы
+void handler(int signo, siginfo_t *info, void *over);
+
+// Завершить все процессы
+void terminate(union sigval arg);
+
+// Готов ли процесс запустить таймер
+volatile bool isReadyToStartTimer = false;
+
+// Готов ли процесс начать чтение
+volatile bool isReadyToStartRead = false;
+
+FILE* file;
 
 int main(int argc, char *argv[]) {
-	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
+
+	std::cout << "M2 running" << std::endl;
+
+	// Задаем диспозицию пользовательскому сигналу,
+	// отвечающему за запуск таймера и начало чтения из именованной памяти
+	sigset_t signalSet;
+	sigemptyset(&signalSet);
+	sigaddset(&signalSet, SIGUSR1);
+
+	struct sigaction act;
+	act.sa_flags = 0;
+	act.sa_mask = signalSet;
+	act.sa_sigaction = &handler;
+
+	sigaction(SIGUSR1, &act, NULL);
+
+	// Инициализируем сигнал
+	int signo;
+	sigset_t timerSignalSet;
+	sigemptyset(&timerSignalSet);
+	sigaddset(&timerSignalSet, SIGUSR2);
+
+	// Размер именнованной памяти
+	size_t sharedMemoryLength = 32;
+
+	// Создаем именованную память
+	int fd = shm_open("/sharemap", O_CREAT | O_RDWR | O_TRUNC, 0);
+	if (fd == -1)
+	{
+		std::cout << "The error occurred while creating share memory." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	ftruncate(fd, sharedMemoryLength);
+
+	// Оображаем именованную память в адресное пространство процесса
+	char* addr = (char*)mmap(0, sharedMemoryLength, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+	if (addr == MAP_FAILED)
+	{
+		std::cout << "The error occurred while mapping shared memory." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Запускаем дочерний процесс на основе модуля L3_M1
+	int cid = spawnl(P_NOWAIT, "L3_M1", "L3_M1", "L3_M1", NULL);
+	if (cid == -1)
+	{
+		std::cout << "The error occurred while starting new process." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Тип уведомления таймера
+	struct sigevent event;
+
+	// Инициализируем уведомление типа "Сигнал"
+	SIGEV_SIGNAL_INIT(&event, SIGUSR2);
+
+	// Id таймера
+	timer_t timerid;
+
+	// Создаем таймер
+	if (timer_create(CLOCK_REALTIME, &event, &timerid) == -1)
+	{
+		std::cout << "The error occurred while creating timer." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Свойства таймера
+	struct itimerspec timer;
+
+	// Задаем свойства таймера
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_nsec = 200000000;
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_nsec = 200000000;
+
+	// Тип уведомления таймера
+	struct sigevent threadEvent;
+
+	// Инициализируем уведомление типа "Создать нить"
+	SIGEV_THREAD_INIT(&threadEvent, terminate, cid, NULL);
+
+	// Id таймера
+	timer_t stopTimerId;
+
+	// Создаем таймер
+	if (timer_create(CLOCK_REALTIME, &threadEvent, &stopTimerId) == -1)
+	{
+		std::cout << "The error occurred while creating timer." << std::endl;
+		std::cout << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Свойства таймера
+	struct itimerspec stopTimer;
+
+	// Задаем свойства таймера
+	stopTimer.it_value.tv_sec = 57;
+	stopTimer.it_value.tv_nsec = 0;
+	stopTimer.it_interval.tv_sec = 0;
+	stopTimer.it_interval.tv_nsec = 0;
+
+	file = fopen("/home/host/trend", "wt");
+
+	// Буффер для записи значения функции и времени
+	char bufferFunctionResult[sharedMemoryLength];
+	char bufferTime[sizeof(double)];
+
+	// Время
+	double time = 0;
+
+	// Ожидаем сигнал для запуска таймера
+	while (!isReadyToStartTimer);
+
+	// Запускаем таймер на считывание значений из именованной память
+	timer_settime(timerid, 0, &timer, NULL);
+
+	// Запускаем таймер на завершение всех процессов
+	timer_settime(stopTimerId, 0, &stopTimer, NULL);
+
+	while (true)
+	{
+		// Ожидаем сигнал для чтения первого значения
+		while (!isReadyToStartRead);
+
+		// Копируем результат функции в именованную память
+		strcpy(bufferFunctionResult, addr);
+
+		//std::cout << "read: " << bufferFunctionResult << std::endl;
+
+		// Преобразуем время к строке
+		sprintf(bufferTime, "%f", time);
+
+		//std::cout << bufferTime << std::endl;
+		//std::cout << bufferFunctionResult << std::endl;
+
+		// Записываем в файл
+		writeToFile(bufferFunctionResult, bufferTime);
+
+		// Ожидаем сигнал
+		sigwait(&timerSignalSet, &signo);
+
+		// Увеличиваем время
+		time += 0.2;
+	}
+
 	return EXIT_SUCCESS;
 }
+
+void handler(int signo, siginfo_t *info, void *over)
+{
+	switch (info->si_code)
+	{
+		case -10:
+			std::cout << "Received signal with code \"READY_TO_START_TIMER\"" << std::endl;
+			isReadyToStartTimer = true;
+			break;
+
+		case -11:
+			std::cout << "Received signal with code \"READY_TO_START_READ\"" << std::endl;
+			isReadyToStartRead = true;
+			break;
+
+		default:
+			std::cout << "Received signal with unknown code" << std::endl;
+	}
+}
+
+void writeToFile(char* functionValue, char* time)
+{
+	char text[200] = "";
+
+	strcat(text, functionValue);
+	strcat(text, "\t");
+	strcat(text, time);
+	strcat(text, "\n");
+
+	fputs(text, file);
+}
+
+void terminate(union sigval arg)
+{
+	std::cout << "Terminate all processes" << std::endl;
+
+	fclose(file);
+
+	kill(arg.sival_int, SIGKILL);
+	kill(getpid(), SIGKILL);
+}
+
